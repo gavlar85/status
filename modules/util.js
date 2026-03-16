19ed289b49f675356dfb80d449207af4dedce8f4
gdresser <52797234+gavlar85@users.noreply.github.com>
2026-03-16 15:58:50 +0000

    Delete modules directory

--- modules/util.js
+++
@@ -1,5 +0,0 @@
-export function uid(prefix="ID") {
-  const rnd = Math.random().toString(36).slice(2, 8).toUpperCase();
-  const t = Date.now().toString(36).slice(-5).toUpperCase();
-  return `${prefix}_${t}${rnd}`;
-}
